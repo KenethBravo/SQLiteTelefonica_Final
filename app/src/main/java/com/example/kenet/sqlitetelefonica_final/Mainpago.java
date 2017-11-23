25ce88825643c66a2f88abc35bad4f29aabf9790
kenetriascos <kenethriascosbravo@gmail.com>
2017-11-22 23:14:22 -0500

    lLISTO TODO DEJA IGUAL EN LA VISTA DE PAGO AHI LE AGREGE UNA LISTA DESPLEGABLE UTILIZA LO MISMO MEN, AHI YA QUEDO FULL FUNCIONANDO

--- app/src/main/java/com/example/kenet/sqlitetelefonica_final/Mainpago.java
+++ app/src/main/java/com/example/kenet/sqlitetelefonica_final/Mainpago.java
@@ -60,6 +60,7 @@ public class Mainpago extends AppCompatActivity {
             {    }
         });
 
+        
         btnrecarga.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
